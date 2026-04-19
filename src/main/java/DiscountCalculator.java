9e151b70fa0c92653339d08d0d24aa13ed6bd6b3
Gomvi19 <victoralejandro4567@hotmail.com>
2026-04-18 21:46:02 -0400

    taking user input

--- src/main/java/DiscountCalculator.java
+++ src/main/java/DiscountCalculator.java
@@ -1,45 +1,24 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.Scanner;
-
 public class DiscountCalculator {
 
     private static final Logger logger = LogManager.getLogger(DiscountCalculator.class);
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        String userId = "user123";
-        String sessionId = "sessionABC";
-
-        logger.info("Starting calculation | userId={} sessionId={}", userId, sessionId);
-
-        try {
-            System.out.print("Enter price: ");
-            double price = scanner.nextDouble();
-
-            System.out.print("Enter discount (%): ");
-            int discount = scanner.nextInt();
-
-            logger.info("User input received | price={} discount={} userId={} sessionId={}",
-                    price, discount, userId, sessionId);
-
-            double discountedPrice = calculateDiscount(price, discount, userId, sessionId);
-
+            double price = 100;
+            int discount = 10;
+            String userId = "user123";
+            String sessionId = "sessionABC";
+            logger.info("Starting calculation | userId={} sessionId={}", userId, sessionId);
+            logger.info("Starting discount calculation...");
+            double discountedPrice = DiscountCalculator.calculateDiscount(price, discount,userId,sessionId);
             logger.info("Calculation complete | originalPrice={} discount={} finalPrice={} userId={} sessionId={}",
-                    price, discount, discountedPrice, userId, sessionId);
-
-            System.out.println("Discounted price: " + discountedPrice);
-
-        } catch (Exception e) {
-            logger.error("Invalid input type entered | userId={} sessionId={}", userId, sessionId);
-            System.out.println("Invalid input. Please enter numeric values.");
-        }
-
-        scanner.close();
+                price, discount, discountedPrice, userId, sessionId);
+            System.out.println("Original price: " + price);
+            System.out.println("Discount: " + discount + "%");
+            System.out.println("Discounted price after discount: " + discountedPrice);
     }
+
 
     public static double calculateDiscount(double price, int percentage, String userId, String sessionId) {
         if (price <= 0 || percentage < 0) {
@@ -47,7 +26,6 @@ public class DiscountCalculator {
                     price, percentage, userId, sessionId);
             return 0;
         }
-
         return price - (price * percentage / 100);
     }
 }
